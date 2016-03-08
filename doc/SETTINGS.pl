63be5b9e9674a9bd9abe77af9ce39b4a7b0578da
Jose F. Morales <jfmcjf@gmail.com>
2016-03-08 15:09:28 +0100

    Simplified lpdoc manual (needs latest ciao version)

--- doc/SETTINGS.pl
+++ doc/SETTINGS.pl
@@ -2,16 +2,6 @@
 
 :- doc(title, "Configuration for FuzzyProlog manual").
 
-datamode(_) :- fail.
-execmode(_) :- fail.
-
-% (not customized)
-bibfile(_) :- fail.
-htmldir(_) :- fail.
-docdir(_) :- fail.
-infodir(_) :- fail.
-mandir(_) :- fail.
-
 filepath := ~fsR(bundle_src('FuzzyProlog')/doc).
 filepath := ~fsR(bundle_src('FuzzyProlog')/lib).
 filepath := ~fsR(bundle_src('FuzzyProlog')). % (for examples/)
@@ -19,18 +9,3 @@ filepath := ~fsR(bundle_src('FuzzyProlog')). % (for examples/)
 output_name := 'FuzzyProlog'.
 
 doc_structure := 'fuzzy/fuzzy_doc'.
- 
-commonopts := no_bugs|no_patches.
-doc_mainopts := ~commonopts.
-doc_compopts := ~commonopts.
-
-index := concept|lib|pred|prop|regtype|decl|author|global.
-
-startpage := 1.
-
-papertype := afourpaper.
-
-libtexinfo := 'yes'.
-
-docformat := texi|ps|pdf|manl|info|html.
-
